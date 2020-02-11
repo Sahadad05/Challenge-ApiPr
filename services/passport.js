f036d2108cc790599bf8b6adaa856d27a0e73489
Sahadad05 <brisapinaki@gmail.com>
2020-02-11 03:12:42 -0600

    driver model updated

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
 
 const Driver = mongoose.model('drivers')
 
@@ -11,6 +12,7 @@ passport.use(new GoogleStrategy({
   callbackURL: '/auth/google/callback'
 }, 
 (accessToken, refreshToken, profile, done) => {
+  new Driver({ googleId: profile.id }).save();
   console.log('access token', accessToken);
   console.log('refresh token', refreshToken);
   console.log('profile', profile);
