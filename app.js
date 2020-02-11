f938116c80e98a9e552c90f8a5c57e363e8e4b63
Sahadad05 <brisapinaki@gmail.com>
2020-02-11 03:05:32 -0600

    app.js uncommented

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-// var express = require("express"),
-//     app = express(),
-//     http = require("http"),
-//     server = http.createServer(app),
-//     bodyParser  = require("body-parser"),
-//     methodOverride = require("method-override");
-//     mongoose = require('mongoose');
-//     Rides = require('./controllers/rides'),
-//     models = require('./models/rides')
+var express = require("express"),
+    app = express(),
+    http = require("http"),
+    server = http.createServer(app),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require('mongoose');
+    Rides = require('./controllers/rides'),
+    models = require('./models/rides')
 
-// app.use(bodyParser.urlencoded({ extended: false }));
-// app.use(bodyParser.json());
-// app.use(methodOverride());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(methodOverride());
 
-// var router = express.Router();
-// router.get('/', function(req, res) {
-//    res.send("Hello World!");
-// });
+var router = express.Router();
+router.get('/', function(req, res) {
+   res.send("Hello World!");
+});
 
-// var rides = express.Router();
+var rides = express.Router();
 
-// rides.route('/rides')
-// .get(Rides.findallRides)
-// .post(Rides.addRide);
+rides.route('/rides')
+.get(Rides.findallRides)
+.post(Rides.addRide);
 
-// rides.route('/rides/:id')
-// .get(Rides.findById)
-// .put(Rides.updateRide)
-// .delete(rides.deleteRide)
+rides.route('/rides/:id')
+.get(Rides.findById)
+.put(Rides.updateRide)
+.delete(rides.deleteRide)
 
 
-// app.use(router);
-// app.use('/api', rides);
+app.use(router);
+app.use('/api', rides);
 
-// mongoose.connect('mongodb://localhost/rides', function(err, res) {
-//   if(err) {
-//     console.log('ERROR: connecting to Database. ' + err);
-//   }
-// })
+mongoose.connect('mongodb://localhost/rides', function(err, res) {
+  if(err) {
+    console.log('ERROR: connecting to Database. ' + err);
+  }
+})
 
-// app.listen(3000, function() {
-//   console.log("Node server running on http://localhost:3000");
-// });
+app.listen(3000, function() {
+  console.log("Node server running on http://localhost:3000");
+});
--- services/passport.js
+++ services/passport.js
@@ -2,6 +2,9 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
 
+const Driver = mongoose.model('drivers')
+
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
